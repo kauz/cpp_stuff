94c6324a2d1d8457f11af4cb4ca6d0f072a5ff2e
kauz <kauzlein@gmail.com>
2020-03-07 15:47:07 +0200

    This is the last time we let minimal commit ascii porn in the comments.

---
+++ 1/1_bot.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+  using namespace std;
+  string name;
+  int age;
+  
+  cout << "- Hello! What is your name?" << endl;
+  cerr << "> ";
+  cin >> name;
+  cout << "- I'm glad to see you, " << name << "!" << endl
+       << "- What is your age?" << endl;
+  cerr << "> ";
+  cin >> age;
+  cout << "- I thought you are " << age - 1
+       << " years old. You look younger!" << endl;
+
+  return 0;
+}
+
+// ./a.out > out.txt < in.txt 2> err.txt
